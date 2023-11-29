564468e44a0e06fcd1e49dfd257710fe5b8e2d98
Anto <antonellamagnano@gmail.com>
2023-11-29 14:38:47 -0300

    add figura a ourservices

--- src/Sections/OurServices/OurServices.jsx
+++ src/Sections/OurServices/OurServices.jsx
@@ -6,6 +6,7 @@ import { GoArrowRight } from "react-icons/go";
 const OurServices = () => {
   return (
     <section className='ourServices'>
+      <img src="/img/elipse-main.svg" alt="" className='elipse-main'/>
         <h2>
         ¿Ya conocés nuestros servicios de asistencia?
         </h2>
