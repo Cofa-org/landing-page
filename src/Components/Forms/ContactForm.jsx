e871c3a9d76a0017a91b6a4f868412ca9e55e5ce
Anto <antonellamagnano@gmail.com>
2023-12-13 11:33:37 -0300

    act msn error forms

--- src/Components/Forms/ContactForm.jsx
+++ src/Components/Forms/ContactForm.jsx
@@ -130,12 +130,16 @@ const ContactForm = ({ type }) => {
 
     if (!values.name) {
       errors.name = 'El nombre no puede estar vacío';
+    }else {
+        errors.name = '';
     }
 
     if (!values.dni) {
       errors.dni = 'El DNI no puede estar vacío';
     }else if (String(values.dni).length !== 8) {
       errors.dni = 'El DNI debe tener 8 dígitos';
+    }else {
+        errors.dni = '';
     }
 
     const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -144,12 +148,28 @@ const ContactForm = ({ type }) => {
       errors.email = 'El email no puede estar vacío';
     } else if (!emailPattern.test(values.email)) {
       errors.email = 'Debe ingresar un email válido';
+    }else {
+        errors.email = '';
     }
 
     if (!values.telephone) {
       errors.telephone = 'El celular no puede estar vacío';
-    } else if (String(values.telephone).length !== 14) {
+    } else if (String(values.telephone).length !== 10) {
       errors.telephone = 'Debe ingresar un celular válido';
+    }else {
+        errors.telephone = '';
+    }
+
+    if(values.situation === "no"){
+        errors.situation = 'Debe elegir una situación laboral';
+    }else {
+        errors.situation = '';
+    }
+
+    if(!values.amount){
+        errors.amount = 'El importe no puede estar vacío';
+    }else {
+        errors.amount = '';
     }
 
     return errors;
--- src/Components/Forms/WorkWithUsForm.jsx
+++ src/Components/Forms/WorkWithUsForm.jsx
@@ -103,12 +103,16 @@ const WorkWithUsForm = () => {
   
       if (!values.name) {
         errors.name = 'El nombre no puede estar vacío';
+      }else {
+          errors.name = '';
       }
   
       if (!values.dni) {
         errors.dni = 'El DNI no puede estar vacío';
       }else if (String(values.dni).length !== 8) {
         errors.dni = 'El DNI debe tener 8 dígitos';
+      }else {
+          errors.dni = '';
       }
   
       const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -117,12 +121,28 @@ const WorkWithUsForm = () => {
         errors.email = 'El email no puede estar vacío';
       } else if (!emailPattern.test(values.email)) {
         errors.email = 'Debe ingresar un email válido';
+      }else {
+          errors.email = '';
       }
   
       if (!values.telephone) {
         errors.telephone = 'El celular no puede estar vacío';
-      } else if (String(values.telephone).length !== 14) {
+      } else if (String(values.telephone).length !== 10) {
         errors.telephone = 'Debe ingresar un celular válido';
+      }else {
+          errors.telephone = '';
+      }
+
+      if(values.situation === "no"){
+          errors.situation = 'Debe elegir una situación laboral';
+      }else {
+          errors.situation = '';
+      }
+
+      if(!values.amount){
+          errors.amount = 'El importe no puede estar vacío';
+      }else {
+          errors.amount = '';
       }
   
       return errors;
