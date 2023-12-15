47f6ff9293f29d0376b25673f03cbfa04f22c830
matu.dev <matu.a.gimenez2002@gmail.com>
2023-12-15 16:01:10 -0300

    act

--- src/Sections/Hero/HeroAssist.jsx
+++ src/Sections/Hero/HeroAssist.jsx
@@ -16,9 +16,9 @@ const HeroAssist = () => {
                     <p>Tu red de seguridad en situaciones imprevistas. Ya sea que necesites atención médica de emergencia, asistencia en el hogar o ayuda para encontrar un médico, COFA Asistencias está acá para ayudarte.</p>
                 </div>
                 <div className='hero-buttons'>
-                    <Link className='primary-btn' to='https://api.whatsapp.com/send/?phone=5491154559017' target='_blank'>Pedir asistencias <GoArrowRight /></Link> 
+                    <Link className='primary-btn' to='https://api.whatsapp.com/send/?phone=5491154559017' target='_blank'>Pedir Asistencias <GoArrowRight /></Link> 
                     <Link to={'/assists/#multi-asistencia'} className='primary-btn btn-score-points'>
-                        Saber mas <GoArrowDown /> 
+                        Saber Más <GoArrowDown /> 
                     </Link>
                 </div>
             </div>
