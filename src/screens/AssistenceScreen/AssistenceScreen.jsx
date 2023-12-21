e5767ca270eeba0929494eb09e9eec2dc041bcd8
Anto <antonellamagnano@gmail.com>
2023-12-21 15:12:23 -0300

    act

--- src/screens/AssistenceScreen/AssistenceScreen.jsx
+++ src/screens/AssistenceScreen/AssistenceScreen.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react'
-import './AssistenceScreen.css'
 import { IoMdArrowRoundForward } from "react-icons/io";
 import { asistencias, asistenciasDesempleo, asistenciasSalud } from '../../data/assistData';
 import { AssistSlider, Footer, HeaderAssist } from '../../Components';
 import { Contact, HeroAssist } from '../../Sections';
 import { Link, useLocation } from 'react-router-dom';
-
-
+import './AssistenceScreen.css'
 
 const AssistenceScreen = () => {
   const { pathname, hash } = useLocation();
@@ -29,6 +27,7 @@ const AssistenceScreen = () => {
     <>
       <HeaderAssist/>
       <HeroAssist/>
+      <>
         <h2 id='multi-asistencia' className='subtitle-assist'>Explora los servicios de Multiasistencia</h2>
         <span className='price-assist'><span className='price-strong'>$1400</span>/Mensuales</span>
         <p className='description-assist'>Los servicios de Multiasistencia cubren asistencia del <strong>hogar</strong>, para <strong>mascotas</strong>, <strong>legal</strong>, <strong>informática</strong>, y asistencia <strong>vehicular</strong>. </p>
@@ -36,7 +35,8 @@ const AssistenceScreen = () => {
         <Link className='go-to-terms' to={'/assists/terminos-multiasistencia'}>
           Términos y Condiciones Particulares de Multiasistencia <IoMdArrowRoundForward/>
         </Link>
-        
+      </>
+      <>
         <h2 id='salud' className='subtitle-assist'>Asistencia en Salud Integral</h2>
         <span className='price-assist'><span className='price-strong'>$700</span>/Mensuales</span>
         <p className='description-assist'>Los servicios cubren asistencia del <strong>Odontología</strong>, para <strong>Nutrición</strong>, <strong>Psicología</strong>, <strong>Clínica médica</strong>, <strong>Servicio de Acompañante</strong>, entre otros. 
@@ -45,14 +45,17 @@ const AssistenceScreen = () => {
         <Link className='go-to-terms' to={'/assists/terminos-salud-integral'}>
           Términos y Condiciones Particulares de Salud <IoMdArrowRoundForward/>
         </Link>
+      </>
+      <>
         <h2 id='desempleo' className='subtitle-assist'>Asistencia en Desempleo</h2>
         <span className='price-assist'><span className='price-strong'>$700</span>/Mensuales</span>
         <AssistSlider asistencias={asistenciasDesempleo}/>
         <Link className='go-to-terms' to={'/assists/terminos-desempleo'}>
           Términos y Condiciones Particulares de Desempleo <IoMdArrowRoundForward/>
         </Link>
-        <Contact/>
-        <Footer/>
+      </>
+      <Contact/>
+      <Footer/>
     </>
   )
 }
