ed62c365047eca68229026617cb8cf55c8765b79
Anto <antonellamagnano@gmail.com>
2024-01-05 13:24:33 -0300

    act estilos asistencias

--- src/screens/AssistenceScreen/AssistenceScreen.jsx
+++ src/screens/AssistenceScreen/AssistenceScreen.jsx
@@ -28,7 +28,7 @@ const AssistenceScreen = () => {
       <HeaderAssist/>
       <HeroAssist/>
       <div>
-        <h3 style={{textAlign:"center",fontSize:"42px",marginBottom:"30px",paddingTop: "6%"}} id='multiasistencia'>Explora los servicios de Multiasistencia</h3>
+        <h3 className='subtitulo-h2-asistencias'  id='multiasistencia'>Explora los servicios de Multiasistencia</h3>
         <span className='price-assist'><span className='price-strong'>$2000</span>/Mensuales</span>
         <p className='description-assist'>Los servicios de Multiasistencia cubren asistencia del <strong>hogar</strong>, para <strong>mascotas</strong>, <strong>legal</strong>, <strong>informática</strong>, y asistencia <strong>vehicular</strong>. </p>
         <AssistSlider asistencias={asistencias}/>
@@ -36,8 +36,8 @@ const AssistenceScreen = () => {
           Términos y Condiciones Particulares de Multiasistencia <IoMdArrowRoundForward/>
         </Link>
       </div>
-      <>
-        <h2 className='subtitulo-h2-asistencias' id='salud'>Asistencia en Salud Integral</h2>
+      <div>
+        <h3 className='subtitulo-h2-asistencias' id='salud'>Asistencia en Salud Integral</h3>
         <span className='price-assist'><span className='price-strong'>$2100</span>/Mensuales</span>
         <p className='description-assist'>Los servicios cubren asistencia del <strong>Odontología</strong>, para <strong>Nutrición</strong>, <strong>Psicología</strong>, <strong>Clínica médica</strong>, <strong>Servicio de Acompañante</strong>, entre otros. 
         </p>
@@ -45,15 +45,15 @@ const AssistenceScreen = () => {
         <Link className='go-to-terms' to={'/assists/terminos-salud-integral'}>
           Términos y Condiciones Particulares de Salud <IoMdArrowRoundForward/>
         </Link>
-      </>
-      <>
-        <h2 className='subtitulo-h2-asistencias' id='desempleo'>Asistencia en Desempleo</h2>
+      </div>
+      <div>
+        <h3 className='subtitulo-h2-asistencias' id='desempleo'>Asistencia en Desempleo</h3>
         <span className='price-assist'><span className='price-strong'>$1100</span>/Mensuales</span>
         <AssistSlider asistencias={asistenciasDesempleo}/>
         <Link className='go-to-terms' to={'/assists/terminos-desempleo'}>
           Términos y Condiciones Particulares de Desempleo <IoMdArrowRoundForward/>
         </Link>
-      </>
+      </div>
       <Contact/>
       <Footer/>
     </>
