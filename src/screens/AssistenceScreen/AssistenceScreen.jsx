6084071118c841ceb939cb79f1d8140518a93037
Anto <antonellamagnano@gmail.com>
2024-01-05 13:18:06 -0300

    act

--- src/screens/AssistenceScreen/AssistenceScreen.jsx
+++ src/screens/AssistenceScreen/AssistenceScreen.jsx
@@ -27,15 +27,15 @@ const AssistenceScreen = () => {
     <>
       <HeaderAssist/>
       <HeroAssist/>
-      <>
-        <h2 className='subtitulo-h2-asistencias' id='multiasistencia'>Explora los servicios de Multiasistencia</h2>
+      <div>
+        <h2 style={{textAlign:"center",fontSize:"42px",marginBottom:"30px",paddingTop: "6%"}} id='multiasistencia'>Explora los servicios de Multiasistencia</h2>
         <span className='price-assist'><span className='price-strong'>$2000</span>/Mensuales</span>
         <p className='description-assist'>Los servicios de Multiasistencia cubren asistencia del <strong>hogar</strong>, para <strong>mascotas</strong>, <strong>legal</strong>, <strong>informática</strong>, y asistencia <strong>vehicular</strong>. </p>
         <AssistSlider asistencias={asistencias}/>
         <Link className='go-to-terms' to={'/assists/terminos-multiasistencia'}>
           Términos y Condiciones Particulares de Multiasistencia <IoMdArrowRoundForward/>
         </Link>
-      </>
+      </div>
       <>
         <h2 className='subtitulo-h2-asistencias' id='salud'>Asistencia en Salud Integral</h2>
         <span className='price-assist'><span className='price-strong'>$2100</span>/Mensuales</span>
