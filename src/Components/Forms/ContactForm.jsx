899907fa755ba37d665a1db1921281e3761fc6be
matu.dev <matu.a.gimenez2002@gmail.com>
2024-01-08 12:33:28 -0300

    validacion de motivo de reclamo agregada

--- src/Components/Forms/ContactForm.jsx
+++ src/Components/Forms/ContactForm.jsx
@@ -8,7 +8,7 @@ dotenv.config(); */
 import './style.css';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineDelete } from "react-icons/ai";
-import { FaArrowRightLong,FaCheck  } from "react-icons/fa6";
+import { FaArrowRightLong, FaCheck } from "react-icons/fa6";
 
 
 
@@ -55,7 +55,7 @@ const MyDropzone = ({ field, form: { setFieldValue } }) => {
               ))}
             </ul>
           </div>
-          <AiOutlineDelete onClick={handleDeleteFiles}/>
+          <AiOutlineDelete onClick={handleDeleteFiles} />
         </div>
       ) : (
         <div {...getRootProps()} className="dropzone">
@@ -71,6 +71,7 @@ const MyDropzone = ({ field, form: { setFieldValue } }) => {
 
 const ContactForm = ({ type }) => {
   const [isSent, setIsSent] = useState(false)
+  const [isSelectorOpen, setIsSelectorOpen] = useState(false)
   const pathname = useLocation()
   const route = pathname.pathname.substring(1)
   const sendMailRequest = async (values) => {
@@ -81,12 +82,12 @@ const ContactForm = ({ type }) => {
     formData.append('email', values.email);
     formData.append('telephone', values.telephone);
     formData.append('message', values.message);
-    if(type == 'RECLAMO'){
+    if (type == 'RECLAMO') {
       formData.append('reason', values.reasonSelected.reason + ' > ' + values.reasonSelected.value)
     }
 
 
-    
+
 
     if (values.files && values.files.buffer) {
       console.log('hola')
@@ -101,30 +102,52 @@ const ContactForm = ({ type }) => {
     console.log('prueba')
     /* https://backend-landing-cofa-production.up.railway.app/mail/ Production */
     /* http://localhost:1000/mail */
-    const response = await fetch('https://backend-landing-cofa-production.up.railway.app/mail/' + type + '/', {
-      method: 'POST',
-      headers: {
+    
+    if (type === 'RECLAMO') {
+ 
+      if (reasonSelected.reason && reasonSelected.value) {
+        const response = await fetch('https://backend-landing-cofa-production.up.railway.app/mail/' + type + '/', {
+          method: 'POST',
+          headers: {
 
-        'Authorization': `Bearer clave-secreta-cofa`,
-      },
-      body: formData,
-    }).then(res =>{
-      if(res.status == 200){
-          setIsSent(true)
+            'Authorization': `Bearer clave-secreta-cofa`,
+          },
+          body: formData,
+        }).then(res => {
+          if (res.status == 200) {
+            setIsSent(true)
+          }
+        })
+
       }
-  })
+    }
+    else{
+      const response = await fetch('https://backend-landing-cofa-production.up.railway.app/mail/' + type + '/', {
+          method: 'POST',
+          headers: {
 
+            'Authorization': `Bearer clave-secreta-cofa`,
+          },
+          body: formData,
+        }).then(res => {
+          if (res.status == 200) {
+            setIsSent(true)
+          }
+        })
+    }
+    
 
   };
 
   const handleSubmit = async (values) => {
-    if(type == 'RECLAMO'){
+    setIsSelectorOpen(true)
+    if (type == 'RECLAMO') {
       const valuesWithSelection = { ...values, reasonSelected: { ...reasonSelected, reason: getReason().title } };
-      sendMailRequest(valuesWithSelection );
-    }else{
-      sendMailRequest( values)
+      sendMailRequest(valuesWithSelection);
+    } else {
+      sendMailRequest(values)
     }
-    
+
   };
 
   const validate = (values) => {
@@ -132,52 +155,59 @@ const ContactForm = ({ type }) => {
 
     if (!values.name) {
       errors.name = 'El nombre no puede estar vacío';
-    }else {
-        errors.name = '';
+    } else {
+      errors.name = '';
     }
 
     if (!values.dni) {
       errors.dni = 'El DNI no puede estar vacío';
-    }else if (String(values.dni).length !== 8) {
+    } else if (String(values.dni).length !== 8) {
       errors.dni = 'El DNI debe tener 8 dígitos';
-    }else {
-        errors.dni = '';
+    } else {
+      errors.dni = '';
     }
 
     const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    console.log(values)
+    if (type === 'RECLAMO') {
+      if (isSelectorOpen && !reasonSelected.reason && !reasonSelected.value) {
+        errors.reason = 'Debe seleccionar una razon'
+      }
+    }
 
     if (!values.email) {
       errors.email = 'El email no puede estar vacío';
     } else if (!emailPattern.test(values.email)) {
       errors.email = 'Debe ingresar un email válido';
-    }else {
-        errors.email = '';
+    } else {
+      errors.email = '';
     }
 
     if (!values.telephone) {
       errors.telephone = 'El celular no puede estar vacío';
     } else if (String(values.telephone).length !== 14) {
       errors.telephone = 'Debe ingresar un celular válido';
-    }else {
-        errors.telephone = '';
+    } else {
+      errors.telephone = '';
     }
 
-    if(!values.message){
+    if (!values.message) {
       errors.message = 'No puedes dejar el mensaje vacio'
     }
-    else if(values.message.split(' ').length < 10){
+    else if (values.message.split(' ').length < 10) {
       errors.message = 'El mensaje debe contener almenos 10 palabras.'
     }
-    else{
+    else {
       errors.message = ''
     }
 
 
-   
-    if(!errors.amount && !errors.situacion && !errors.dni && !errors.email ){
+
+    if (!errors.amount && !errors.situacion && !errors.dni && !errors.email) {
       return false
-    }else{
-        return errors;
+    } else {
+      return errors;
     }
   };
 
@@ -256,27 +286,29 @@ const ContactForm = ({ type }) => {
   const [openSelector, setOpenSelector] = useState(false);
 
   const handleSelectOption = (reason) => {
+    setIsSelectorOpen(true)
     setReasonSelected({ ...reasonSelected, reason: reason, value: null });
   };
 
   const handleSelectOptionValue = (value) => {
 
-    if(value == 'rollback'){
-      setReasonSelected({reason: null, value: null})
+    if (value == 'rollback') {
+      setReasonSelected({ reason: null, value: null })
     }
-    else{
+    else {
       setReasonSelected({ ...reasonSelected, value: value });
     }
-    
+
   };
 
   const getReason = () => {
     return reasons.find(reason => reason.name === reasonSelected.reason);
   };
-  useEffect(() =>{
+  useEffect(() => {
     setOpenSelector(false)
-  },[ reasonSelected.value] )
-  
+  }, [reasonSelected.value])
+  console.log(isSelectorOpen, !openSelector, reasonSelected.reason, reasonSelected.value)
+  console.log((isSelectorOpen && !openSelector) && (!reasonSelected.reason || !reasonSelected.value))
   return (
     <div className="quejas-sugerencias">
       <Formik
@@ -296,73 +328,81 @@ const ContactForm = ({ type }) => {
           <div className="input-container">
             <label>Nombre Completo</label>
             <Field name="name" type="text" placeholder="Nombre y apellido" />
-            <ErrorMessage name="name" component="div" className="error-message"/>
+            <ErrorMessage name="name" component="div" className="error-message" />
           </div>
 
           <div className="input-container">
             <label>D.N.I</label>
             <Field name="dni" type="number" placeholder='11222333' />
-            <ErrorMessage name="dni" component="div" className="error-message"/>
+            <ErrorMessage name="dni" component="div" className="error-message" />
           </div>
 
           <div className="input-container">
             <label>Correo electrónico</label>
             <Field name="email" type="email" placeholder="nombre123@gmail.com" />
-            <ErrorMessage name="email" component="div" className="error-message"/>
+            <ErrorMessage name="email" component="div" className="error-message" />
           </div>
 
           <div className="input-container">
             <label htmlFor="telephone">Celular</label>
             <Field name="telephone" type="text" id="telephone" placeholder='1122223333' />
-            <ErrorMessage name="telephone" component="div" className="error-message"/>
+            <ErrorMessage name="telephone" component="div" className="error-message" />
           </div>
-          
-      {
 
-        type === 'RECLAMO'  && (
-          <div className="input-container input-container-100">
-            <label htmlFor="reason">Motivo:</label>
-            <div className='selector-input-container'>
-              <span
-                className='selector-input'
-                onClick={() => setOpenSelector(!openSelector)}
-              >
-                {reasonSelected.reason ? <span>{getReason().title} {'>'}</span> : <span>Selecciona tu motivo</span>}
-                <span>{
-                reasonSelected.value &&  reasonSelected.value !== 'rollback' && reasonSelected.value
-                }
-                </span>
-              </span>
-              <div className={openSelector ? 'dropdown-list-reason' : 'dropdown-list-reason no-visible'}>
-                {reasonSelected.reason && reasonSelected.value !== 'rollback'
-                  ? <div className='reason-list-values'>
-                    {getReason()?.values.map(value => (
-                      <div onClick={() => handleSelectOptionValue(value)}>
-                        {value}
+          {
+
+            type === 'RECLAMO' && (
+              <div className="input-container input-container-100">
+                <label htmlFor="reason">Motivo:</label>
+                <div className='selector-input-container'>
+                  <span
+                    className='selector-input'
+                    onClick={() => setOpenSelector(!openSelector)}
+                  >
+                    {reasonSelected.reason ? <span>{getReason().title} {'>'}</span> : <span>Selecciona tu motivo</span>}
+                    <span>{
+                      reasonSelected.value && reasonSelected.value !== 'rollback' && reasonSelected.value
+                    }
+                    </span>
+                  </span>
+                  <div className={openSelector ? 'dropdown-list-reason' : 'dropdown-list-reason no-visible'}>
+                    {reasonSelected.reason && reasonSelected.value !== 'rollback'
+                      ? <div className='reason-list-values'>
+                        {getReason()?.values.map(value => (
+                          <div onClick={() => handleSelectOptionValue(value)}>
+                            {value}
+                          </div>
+                        ))}
+                        <div onClick={() => handleSelectOptionValue('rollback')}>Volver</div>
                       </div>
-                    ))}
-                    <div onClick={() => handleSelectOptionValue('rollback')}>Volver</div>
+                      : reasons.map((reason) => (
+                        <div className={openSelector ? 'reason-list' : 'reason-list no-visible'} onClick={() => handleSelectOption(reason.name)}>
+                          <span>{reason.title}</span>
+                          <FaChevronRight />
+                        </div>
+                      ))
+                    }
                   </div>
-                  : reasons.map((reason) => (
-                    <div className={openSelector ? 'reason-list' : 'reason-list no-visible'} onClick={() => handleSelectOption(reason.name)}>
-                      <span>{reason.title}</span>
-                      <FaChevronRight />
-                    </div>
-                  ))
+                </div>
+
+                {
+                  (isSelectorOpen && !openSelector) && (!reasonSelected.reason || !reasonSelected.value) &&
+                  <div className="error-message" >
+                    Debes seleccionar una razon.
+                  </div>
                 }
               </div>
-            </div>
-          </div>
-        )}
+            )}
+
 
           <div className="input-container input-container-100">
             <label htmlFor="message">Mensaje:</label>
 
             <span className='message-item'><span className='circle-item'></span>{EXPLICACION_MENSAJE[type]}</span>
-            <Field as="textarea" name="message" id="message" placeholder={MESSAGES[type]} /* maxLength={255} */  />
+            <Field as="textarea" name="message" id="message" placeholder={MESSAGES[type]} /* maxLength={255} */ />
             <ErrorMessage name="message" component="div" className="error-message" />
           </div>
-        
+
 
           <div className="input-container input-container-100">
             <Field name="files" component={MyDropzone} />
@@ -370,11 +410,11 @@ const ContactForm = ({ type }) => {
           </div>
 
           <div className="submit">
-          {isSent ?  <span className='sent-message'>Enviado <FaCheck /></span> : <button type='submit'>Enviar <FaArrowRightLong /></button>}
+            {isSent ? <span className='sent-message'>Enviado <FaCheck /></span> : <button type='submit'>Enviar <FaArrowRightLong /></button>}
           </div>
 
 
-    </Form>
+        </Form>
       </Formik >
     </div >
   );
