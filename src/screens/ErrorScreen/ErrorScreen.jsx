2a9dff429a35e283fa22bb3e36391ecbd21156b2
Anto <antonellamagnano@gmail.com>
2024-01-17 14:48:48 -0300

    act

--- src/screens/ErrorScreen/ErrorScreen.jsx
+++ src/screens/ErrorScreen/ErrorScreen.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import "./ErrorScreen.css"
+import { Header } from '../../Components';
 
 const ErrorScreen = () => {
   return (
-    <div className='container-error'>
-        <img src="/img/robot-error.svg" alt="Svg error" />
-        <h2>Ups! Algo salió mal</h2>
-        <p>Recargá la página. Si el error persiste, comunicate con nosotros para resolverlo.</p>
-        <div>
-            <span className='email-error'>consultas@cofa.com.ar</span>
-            <span className='tel-error'>+5491154559017</span>
+    <>
+        <Header/>
+        <div className='container-error'>
+            
+            <img src="/img/robot-error.svg" alt="Svg error" />
+            <h2>Ups! Algo salió mal</h2>
+            <p>Recargá la página. Si el error persiste, comunicate con nosotros para resolverlo.</p>
+            <div>
+                <span className='email-error'>consultas@cofa.com.ar</span>
+                <span className='tel-error'>+5491154559017</span>
+            </div>
+            
+            
         </div>
-        
-        
-    </div>
+    </>
   );
 };
 
