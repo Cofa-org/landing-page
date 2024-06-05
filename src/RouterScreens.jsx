10ebe63b218006761ca21736f763ddbc6c58c517
Anto Magnano <antonellamagnano@gmail.com>
2024-06-05 17:36:55 -0300

    /prestamos

--- src/RouterScreens.jsx
+++ src/RouterScreens.jsx
@@ -10,8 +10,8 @@ import { FormWorkWithUs } from './Sections'
 const RouterScreens = () => {
   return (
     <Routes>
-        <Route path='/' element={<HomeScreen/>}/>
-        <Route path='/inicio' element={<HomeScreen/>}/>
+        <Route path='/' element={<HomeScreen/>}/> {/* TO DO: /prestamos */}
+        <Route path='/inicio' element={<HomeScreen/>}/> {/* TO DO: /prestamos */}
         <Route path='/terminos-y-condiciones' element={<TermsScreen/>} />
         <Route path='/politicas-de-privacidad' element={<PrivacyPoliciesScreen/>} />
         <Route path='/puntos-cofa' element={<PointsScreen/>}/>
@@ -20,14 +20,13 @@ const RouterScreens = () => {
         <Route path='/arrepentimiento' element={<RegretOrDischargeScreen/>} /> {/* arrepentimiento */}
         <Route path='/reclamos' element={<ComplaintsScreen/>}/> {/* reclamos */}
         <Route path='/quejas' element={<QuejasScreen/>}/> {/* quejas */}
-        {/* <Route path='/asistencias' element={<AssistenceScreen/>}/> */}
         <Route path='/terminos-y-condiciones-puntos-cofa' element={<TermsPointsScreen/>} />
         <Route path='/trabaja-con-nosotros' element={<FormWorkWithUs/>}/>
         <Route path='/el-mejor-trato' element={<ElMejorTratoScreen />}/>
+        {/* <Route path='/asistencias' element={<AssistenceScreen/>}/> */}
         {/* <Route path='/asistencias/terminos-multiasistencia' element={<TermsV2 type={'MULTIASISTENCIA'}/>}/>
         <Route path='/asistencias/terminos-salud-integral' element={<TermsV2 type={'SALUDINTEGRAL'}/>}/>
         <Route path='/asistencias/terminos-desempleo' element={<TermsV2 type={'DESEMPLEO'}/>}/> */}
-        <Route path='/terminos-' element={<TermsV2 type={'SALUDINTEGRAL'}/>}/>
         <Route path='*' element={<ErrorScreen/>}/>
     </Routes>
   )
