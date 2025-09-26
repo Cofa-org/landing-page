eea52335ca8bf5be25736f39b8ce5a92cab6e97d
MatiasJMaranga <marangamatias@gmail.com>
2025-09-25 23:57:41 -0300

    build

--- src/config.js
+++ src/config.js
@@ -10,8 +10,7 @@ const VITE_URL_LOCAL = import.meta.env.VITE_URL_LOCAL;
 const VITE_SUPABASE_URL =
   import.meta.env.VITE_SUPABASE_URL || "https://qofkplavgmamstbvzluw.supabase.co";
 const VITE_SUPABASE_ANON_KEY =
-  import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZmtwbGF2Z21hbXN0YnZ6bHV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MTQ4MjksImV4cCI6MjAyNjM5MDgyOX0.jWhINTCMNJem7q5v_Ot-Dnr8hRnbP92Ns5ljvkXWzyA";
+  import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZmtwbGF2Z21hbXN0YnZ6bHV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MTQ4MjksImV4cCI6MjAyNjM5MDgyOX0.jWhINTCMNJem7q5v_Ot-Dnr8hRnbP92Ns5ljvkXWzyA";
 
 export {
   VITE_COFA_AUTH_URL,
