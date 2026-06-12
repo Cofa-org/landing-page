d729d89c2aff8a9155cba8ae30b26e22439576bc
MatiasJMaranga <marangamatias@gmail.com>
2026-06-12 18:45:42 -0300

    Se dehabilita el boton quiero mi prestamo luego de hacer click sobre el

--- src/screens/registrationSim/hooks/useWelcomeStep.js
+++ src/screens/registrationSim/hooks/useWelcomeStep.js
@@ -1,19 +1,26 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import LeadRegistrationService from "../../../services/leadRegistrationService.js";
 
 export const useWelcomeStep = () => {
+  const [onboardingCompletado, setOnboardingCompletado] = useState(false);
   const handleWelcomeComplete = useCallback(async (leadId) => {
     try {
       if (leadId) {
         const result = await LeadRegistrationService.onBoardingCompleto(leadId);
+
         if (result.success) {
-          window.open("http://wa.me/5491137570853?text=Hola!%20Quiero%20mi%20pr%C3%A9stamo!!%20%3Ablush%3A", "_blank", "noopener,noreferrer");
+          window.open(
+            "http://wa.me/5491137570853?text=Hola!%20Quiero%20mi%20pr%C3%A9stamo!!%20%3Ablush%3A",
+            "_blank",
+            "noopener,noreferrer",
+          );
         }
+        setOnboardingCompletado(true);
       }
     } catch (err) {
       console.error("Error al completar onboarding:", err);
     }
   }, []);
 
-  return { handleWelcomeComplete };
+  return { handleWelcomeComplete, onboardingCompletado };
 };
