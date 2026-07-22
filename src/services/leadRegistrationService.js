385c46cef23d7533577a37b794983c3afa4fddfa
agente <agente@local>
2026-07-22 16:13:17 -0300

    feat(service): enviar selectedCuit en crearLead cuando aplica (SUP-9)

--- src/services/leadRegistrationService.js
+++ src/services/leadRegistrationService.js
@@ -7,7 +7,7 @@ import { COOKIE_LEAD_TOKEN_CONFIG } from "../constants/LOAN_SIM.js";
 export default class LeadRegistrationService {
 
   static async crearLead(
-    { dni, turnstileToken, huella_dispositivo, request_id, celular },
+    { dni, turnstileToken, huella_dispositivo, request_id, celular, selectedCuit },
     signal = null,
   ) {
     try {
@@ -18,6 +18,7 @@ export default class LeadRegistrationService {
         huella_dispositivo,
         request_id,
         celular,
+        selectedCuit,
       };
       const response = await HttpApi(
         url,
